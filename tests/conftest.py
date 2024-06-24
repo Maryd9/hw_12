a8b1928ffe3fb18d4c8557cbc7c7de1861fc2dbe
masha <masha-krim@yandex.ru>
2024-06-25 01:32:01 +0500

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import os
 import pytest
-from selene import Browser, Config, browser
+from selene import browser
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -40,11 +40,11 @@ def setup_browser(request):
             "enableVideo": True
         }
     }
-    options.capabilities.update(selenoid_capabilities)
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
 
-    selenoid_login = os.getenv("LOGIN")
-    selenoid_pass = os.getenv("PASS")
-    selenoid_url = os.getenv("URL")
+    options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
@@ -53,7 +53,7 @@ def setup_browser(request):
 
     browser.config.driver = driver
 
-    yield
+    yield browser
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
